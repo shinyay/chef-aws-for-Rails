2c9794e1c48697652c4c79164148de2a48616eb3
shinyay <shinyay@jp.ibm.com>
2013-07-19 10:52:34 +0900

    Modify node.json that configures global version of ruby as 2.0

--- aws-kitchen/site-cookbooks/base-packages/attributes/default.rb
+++ aws-kitchen/site-cookbooks/base-packages/attributes/default.rb
@@ -1,30 +1,31 @@
 node.default[:package] = %w{
   sysstat
   mosh
-  autoconf 
-  automake 
-  bison
   bzip2-devel
-  curl
   curl-devel
   freetype-devel
   gcc
-  gcc-c++
   httpd-devel
-  libffi-devel
   libjpeg-devel
   libpng-devel
-  libtool
-  libxml2-devel
-  libxslt-devel
   libyaml
   libyaml-devel
-  make
   ncurses-devel
-  openssl-devel
-  readline-devel
   tree
-  zlib-devel
 }
 
 # tk-devel
+# The following packages have been defined in the ruby_build.
+# gcc-c++
+# zlib-devel
+# libffi-devel
+# libxml2-devel
+# libxslt-devel
+# autoconf
+# readline-devel
+# openssl-devel
+# make
+# automake 
+# libtool
+# bison
+# curl
